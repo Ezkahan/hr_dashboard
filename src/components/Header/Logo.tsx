2855dbf9789dbbd649c27466290b83337f601a8f
ezkahan <ezkahan.dev@gmail.com>
2023-04-28 22:25:48 +0500

    project start

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -1,7 +1,5 @@
 const Logo: React.FC = () => {
-    return (
-        <h1 className="text-2xl text-center p-3 font-bold">ORLAN</h1>
-    )
-}
+  return <h1 className="text-2xl text-center p-3 font-bold">HR dashboard</h1>;
+};
 
-export default Logo
+export default Logo;
--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,26 +1,24 @@
-import { Navigate } from 'react-router-dom';
-import IChildren from '../common/interfaces/IChildren'
-import MainContainer from '../components/Main/MainContainer'
-import Sidebar from '../components/Navigation/Sidebar'
-import { Toaster } from "react-hot-toast"
-import ReactTooltip from "react-tooltip"
-import Cookie from 'js-cookie'
+import { Navigate } from "react-router-dom";
+import IChildren from "../common/interfaces/IChildren";
+import MainContainer from "../components/Main/MainContainer";
+import Sidebar from "../components/Navigation/Sidebar";
+import { Toaster } from "react-hot-toast";
+import ReactTooltip from "react-tooltip";
+import Cookie from "js-cookie";
 
-const AppLayout = ({children}: IChildren) => {
-    const token = Cookie.get('orlan_token');
+const AppLayout = ({ children }: IChildren) => {
+  const token = Cookie.get("orlan_token");
 
-    return (
-        <>
-        {token ? "" : <Navigate to="/login" />}
-        <section className="font-montserrat-medium">
-            <Toaster />
-            <ReactTooltip className="font-montserrat-bold" />
-            <Sidebar />
-            <MainContainer>
-                { children }
-            </MainContainer>
-        </section>
-        </>
-    )
-}
-export default AppLayout
+  return (
+    <>
+      {token ? "" : <Navigate to="/login" />}
+      <section className="font-montserrat-medium">
+        <Toaster />
+        <ReactTooltip className="font-montserrat-bold" />
+        <Sidebar />
+        <MainContainer>{children}</MainContainer>
+      </section>
+    </>
+  );
+};
+export default AppLayout;
